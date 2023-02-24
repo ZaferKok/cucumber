7dc83cff386572234507e148df32ec0d102ffbcc
ZaferKok <zaferkok111@gmail.com>
2023-02-25 00:10:10 +0100

    ikinci commit

--- src/test/java/stepdefinitions/Hooks.java
+++ src/test/java/stepdefinitions/Hooks.java
@@ -19,12 +19,15 @@ public class Hooks {
 
     @After
     public void teardown(Scenario scenario) {
+        // Aşağıdaki kod ile aldığımız SCREENSHOT'lar çok fazla yer kaplar.
+        // Onun için bu resimler GitHub'a Push edilmemelidir.
+        // Bunu şu şekilde yaparız. test-output klasörüne sağ tıklayıp en aşağıdaki 'Mark Directory as' seçeneği seçilir ve EXCLUDUED işaretlenir.
         byte[] picture;
-        if (scenario.isFailed()) {
+        if (scenario.isFailed()) { // Burada eğer bir testimiz fail ederse Screenshot alabiliyoruz.
             picture = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
             scenario.attach(picture, "image/png", "failed" + scenario.getName());
         }
-        else {
+        else { // Burada eğer testimiz fail etmese bile Screenshot alabiliyoruz. Onun için aşağıdaki else kodu içeriğinin yorum olarak kalmasında fayda var.
             picture = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
             scenario.attach(picture, "image/png", "passed" + scenario.getName());
         }
